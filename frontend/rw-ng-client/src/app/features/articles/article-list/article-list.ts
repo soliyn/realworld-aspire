79ddc6264315a9459cea6aafe795ce9edc5ba519
Sergiy Oliynyk <olen33@gmail.com>
2025-10-22 20:29:22 +0300

    Use declarative approach.

--- frontend/rw-ng-client/src/app/features/articles/article-list/article-list.ts
+++ frontend/rw-ng-client/src/app/features/articles/article-list/article-list.ts
@@ -1,7 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, startWith, catchError, of } from 'rxjs';
 import { ArticleListItem } from '../article-list-item/article-list-item';
 import { ArticlesService } from '../articles.service';
 import { Article } from '../../../core/models/article.model';
+
+type ArticlesState = {
+  articles: Article[];
+  isLoading: boolean;
+};
 
 @Component({
   selector: 'app-article-list',
@@ -9,28 +16,20 @@ import { Article } from '../../../core/models/article.model';
   templateUrl: './article-list.html',
   styleUrl: './article-list.scss',
 })
-export class ArticleList implements OnInit {
+export class ArticleList {
   private articlesService = inject(ArticlesService);
 
-  articles = signal<Article[]>([]);
-  isLoading = signal(false);
+  private articlesState = toSignal<ArticlesState>(
+    this.articlesService.getArticles().pipe(
+      map((response) => ({ articles: response.articles, isLoading: false })),
+      startWith({ articles: [], isLoading: true }),
+      catchError(() => of({ articles: [], isLoading: false }))
+    ),
+    { requireSync: true }
+  );
 
-  ngOnInit(): void {
-    this.loadArticles();
-  }
-
-  loadArticles(): void {
-    this.isLoading.set(true);
-    this.articlesService.getArticles().subscribe({
-      next: (response) => {
-        this.articles.set(response.articles);
-        this.isLoading.set(false);
-      },
-      error: () => {
-        this.isLoading.set(false);
-      },
-    });
-  }
+  articles = computed(() => this.articlesState().articles);
+  isLoading = computed(() => this.articlesState().isLoading);
 
   onFavoriteToggle(article: Article): void {
     // Handle favorite toggle - to be implemented
